768e320a535d224fa5c5f25fcd22e68cfc9552b4
Lee, Jeong Han <jeonghan.lee@gmail.com>
2025-03-27 17:56:41 -0700

    add echo in tcpserver-socat.bash

--- simulator/tcpserver-socat.bash
+++ simulator/tcpserver-socat.bash
@@ -2,8 +2,6 @@
 
 PORT=9399
 
-socat TCP-LISTEN:${PORT},reuseaddr,fork SYSTEM:'./connection_handler.sh'
-
-#'read -r command \
-#echo "Received $command from $SOCAT_PEERADDR:$SOCAT_PEERPORT" >&2 \
-#echo "$command"'
+socat TCP-LISTEN:${PORT},reuseaddr,fork SYSTEM:'read -r command \
+echo "Received $command from $SOCAT_PEERADDR:$SOCAT_PEERPORT" >&2 \
+echo "$command"'
