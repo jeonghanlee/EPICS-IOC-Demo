33229736d2636fc252422e8acb8b984890cf2cd6
Lee, Jeong Han <jeonghan.lee@gmail.com>
2025-09-11 10:20:23 -0700

    update according 03.02 at PAS-demo

---
+++ iocBoot/ioctraining-jeonglee-Demo/st.alarm2.cmd
@@ -0,0 +1,34 @@
+#!../../bin/linux-x86_64/jeonglee-Demo
+
+< envPaths
+
+epicsEnvSet("DB_TOP", "$(TOP)/db")
+
+epicsEnvSet("STREAM_PROTOCOL_PATH", "$(DB_TOP)")
+
+# Define macros for the overall device/IOC
+epicsEnvSet("PREFIX_MACRO", "MYDEMO:")
+epicsEnvSet("DEVICE_MACRO", "TC32:") # Using TC32 for this example
+
+epicsEnvSet("IOCNAME", "training-jeonglee-Demo")
+epicsEnvSet("IOC", "ioctraining-jeonglee-Demo")
+
+dbLoadDatabase "$(TOP)/dbd/jeonglee-Demo.dbd"
+jeonglee_Demo_registerRecordDeviceDriver pdbbase
+
+cd "${TOP}/iocBoot/${IOC}"
+
+epicsEnvSet("ASYN_PORT_NAME", "LocalTCPServer")
+epicsEnvSet("TARGET_HOST",    "127.0.0.1")
+epicsEnvSet("TARGET_PORT",    "9399")
+drvAsynIPPortConfigure("$(ASYN_PORT_NAME)", "$(TARGET_HOST):$(TARGET_PORT)", 0, 0, 0)
+
+asynOctetSetInputEos("$(ASYN_PORT_NAME)", 0, "\n")
+asynOctetSetOutputEos("$(ASYN_PORT_NAME)", 0, "\n")
+
+dbLoadRecords("$(DB_TOP)/TC-32-alarm1.db", "P=$(PREFIX_MACRO),R=$(DEVICE_MACRO),PORT=$(ASYN_PORT_NAME)")
+
+iocInit
+
+ClockTime_Report
+
